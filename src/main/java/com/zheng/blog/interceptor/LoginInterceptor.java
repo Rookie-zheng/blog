ea47b2b639fc5b6aec68a6fe6de5dd22d4d35db5
rookie-zheng <944699017@qq.com>
2019-11-04 01:25:49 +0800

    拦截器设置

---
+++ src/main/java/com/zheng/blog/interceptor/LoginInterceptor.java
@@ -0,0 +1,21 @@
+package com.zheng.blog.interceptor;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginInterceptor extends HandlerInterceptorAdapter {
+
+    //方法将在请求处理之前进行调用 拦截器
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler) throws Exception {
+        if(request.getSession().getAttribute("user") == null){
+            response.sendRedirect("/admin");
+            return false;
+        }
+        return true;
+    }
+}
