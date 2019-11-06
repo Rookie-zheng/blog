ef6ae858bced45016eefd61d419d1fd2c414f78c
rookie-zheng <944699017@qq.com>
2019-11-06 15:03:20 +0800

    部分修改

--- src/main/java/com/zheng/blog/web/admin/TypeController.java
+++ src/main/java/com/zheng/blog/web/admin/TypeController.java
@@ -41,6 +41,7 @@ public class TypeController {
     }
 
     @GetMapping("/types/{id}/input")
+    @NoRepeatSubmit
     public String editInput(@PathVariable Long id, Model model) {
         model.addAttribute("type", typeService.getType(id));
         return "admin/types-input";
