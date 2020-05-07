3ab7a4778336f0701fc9c170307775c0941761b3
rookie-zheng <944699017@qq.com>
2020-05-07 10:10:36 +0800

    修改切面

--- src/main/java/com/zheng/blog/aspect/LogAspect.java
+++ src/main/java/com/zheng/blog/aspect/LogAspect.java
@@ -17,7 +17,7 @@ public class LogAspect {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Pointcut("execution(* com.lrm.web.*.*(..))")
+    @Pointcut("execution(* com.zheng.blog.web.*.*(..))")
     public void log() {}
 
 
